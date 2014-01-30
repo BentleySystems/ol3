BentleySystems/ol3

commit ff0131c6b5edb28c82496b73279e77c2491aab14

Convert ol.MapBrowserEvent#get* methods into properties

src/ol/interaction/touchinteraction.js
@@ -81,7 +81,7 @@ ol.interaction.Touch.isTouchEvent_ = function(mapBrowserEvent) {
 ol.interaction.Touch.prototype.updateTrackedTouches_ =
     function(mapBrowserEvent) {
   if (ol.interaction.Touch.isTouchEvent_(mapBrowserEvent)) {
-    var event = mapBrowserEvent.browserEvent.getBrowserEvent();
+    var event = mapBrowserEvent.browserEvent;
     if (goog.isDef(event.targetTouches)) {
       // W3C touch events
       this.targetTouches = event.targetTouches;

examples/drag-and-drop-image-vector.js
@@ -151,6 +151,5 @@ $(map.getViewport()).on('mousemove', function(evt) {
 });
 
 map.on('singleclick', function(evt) {
-  var pixel = evt.getPixel();
-  displayFeatureInfo(pixel);
+  displayFeatureInfo(evt.pixel);
 });

examples/drag-and-drop.js
@@ -148,6 +148,5 @@ $(map.getViewport()).on('mousemove', function(evt) {
 });
 
 map.on('singleclick', function(evt) {
-  var pixel = evt.getPixel();
-  displayFeatureInfo(pixel);
+  displayFeatureInfo(evt.pixel);
 });

examples/gpx.js
@@ -89,6 +89,5 @@ $(map.getViewport()).on('mousemove', function(evt) {
 });
 
 map.on('singleclick', function(evt) {
-  var pixel = evt.getPixel();
-  displayFeatureInfo(pixel);
+  displayFeatureInfo(evt.pixel);
 });

examples/icon.js
@@ -70,7 +70,7 @@ map.addOverlay(popup);
 
 // display popup on click
 map.on('singleclick', function(evt) {
-  var feature = map.forEachFeatureAtPixel(evt.getPixel(),
+  var feature = map.forEachFeatureAtPixel(evt.pixel,
       function(feature, layer) {
         return feature;
       });

examples/igc.js
@@ -111,8 +111,7 @@ $(map.getViewport()).on('mousemove', function(evt) {
 });
 
 map.on('singleclick', function(evt) {
-  var coordinate = evt.getCoordinate();
-  displaySnap(coordinate);
+  displaySnap(evt.coordinate);
 });
 
 var imageStyle = new ol.style.Circle({

examples/image-vector-layer.js
@@ -95,6 +95,5 @@ $(map.getViewport()).on('mousemove', function(evt) {
 });
 
 map.on('singleclick', function(evt) {
-  var pixel = evt.getPixel();
-  displayFeatureInfo(pixel);
+  displayFeatureInfo(evt.pixel);
 });

examples/kml-earthquakes.js
@@ -91,5 +91,5 @@ $(map.getViewport()).on('mousemove', function(evt) {
 });
 
 map.on('singleclick', function(evt) {
-  displayFeatureInfo(evt.getPixel());
+  displayFeatureInfo(evt.pixel);
 });

commit b947c577b685ec4b0603b95574d73cdbbe98db0e

Fix the touch interactions
This fixes a bug introduced by https://github.com/openlayers/ol3/commit/ff0131c6b5edb28c82496b73279e77c2491aab14.  Thanks @gjn for reporting the problem.

src/ol/interaction/touchinteraction.js
@@ -81,7 +81,7 @@ ol.interaction.Touch.isTouchEvent_ = function(mapBrowserEvent) {
 ol.interaction.Touch.prototype.updateTrackedTouches_ =
     function(mapBrowserEvent) {
   if (ol.interaction.Touch.isTouchEvent_(mapBrowserEvent)) {
-    var event = mapBrowserEvent.browserEvent;
+    var event = mapBrowserEvent.originalEvent;
     if (goog.isDef(event.targetTouches)) {
       // W3C touch events
       this.targetTouches = event.targetTouches;